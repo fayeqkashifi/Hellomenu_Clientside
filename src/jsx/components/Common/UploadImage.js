4581a02d4d1c0e6ad3039d49eb9cb4bf8d4d4e3b
Mohammad Faiq Kashifi <fayeqkashifi@gmail.com>
2022-05-31 13:36:39 +0430

    photo validation

--- src/jsx/components/Common/UploadImage.js
+++ src/jsx/components/Common/UploadImage.js
@@ -3,19 +3,59 @@ import ImageUploading from "react-images-uploading";
 import DeleteIcon from "@mui/icons-material/Delete";
 import IconButton from "@mui/material/IconButton";
 import BrowserUpdatedIcon from "@mui/icons-material/BrowserUpdated";
+import axios from "axios";
+
 const UploadImage = (props) => {
   const { images, setImages, values, urlPath } = props;
   const onChangeTest = (imageList, addUpdateIndex) => {
     console.log(imageList);
     setImages(imageList);
   };
+  // let url =
+  //   "https://cdn.shopify.com/s/files/1/0234/8017/2591/products/young-man-in-bright-fashion_925x_f7029e2b-80f0-4a40-a87b-834b9a283c39.jpg";
+  // const toDataURL = (url) =>
+  //   fetch(url)
+  //     .then((response) => response.blob())
+  //     .then(
+  //       (blob) =>
+  //         new Promise((resolve, reject) => {
+  //           const reader = new FileReader();
+  //           reader.onloadend = () => resolve(reader.result);
+  //           reader.onerror = reject;
+  //           reader.readAsDataURL(blob);
+  //         })
+  //     );
+
+  // function dataURLtoFile(dataurl, filename) {
+  //   var arr = dataurl.split(","),
+  //     mime = arr[0].match(/:(.*?);/)[1],
+  //     bstr = atob(arr[1]),
+  //     n = bstr.length,
+  //     u8arr = new Uint8Array(n);
+  //   while (n--) {
+  //     u8arr[n] = bstr.charCodeAt(n);
+  //   }
+  //   return new File([u8arr], filename, { type: mime });
+  // }
 
   useEffect(() => {
     const valuePush = [];
     console.log(values);
     if (values !== undefined && values !== null) {
       if (values.length > 0) {
-        values.map((v) => {});
+        values.map((v) => {
+          let url =
+            "http://127.0.0.1:8000/images/branches/1653889971775266796.jpg";
+          const res = axios.get(url);
+          console.log(res);
+
+          // toDataURL(url).then((dataUrl) => {
+          //   console.log("Here is Base64 Url", dataUrl);
+          //   var fileData = dataURLtoFile(dataUrl, "imageName.jpg");
+          //   console.log("Here is JavaScript File Object", fileData);
+          //   // fileArr.push(fileData);
+          // });
+        });
 
         // setImages(valuePush);
       }
