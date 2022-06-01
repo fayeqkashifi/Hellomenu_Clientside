7a32bb29871849d663750264edea47a2b3be90f9
Mohammad Faiq Kashifi <fayeqkashifi@gmail.com>
2022-06-01 13:55:53 +0430

    images and videos validation

--- src/jsx/components/Branches/AddBranch.js
+++ src/jsx/components/Branches/AddBranch.js
@@ -23,31 +23,7 @@ import PhoneInput, {
 } from "react-phone-number-input";
 import "react-phone-number-input/style.css";
 import UploadImage from "../Common/UploadImage";
-export function checkIfFilesAreTooBig(files?: [File]): boolean {
-  let valid = true;
-  if (files) {
-    console.log(files);
-    files.map((file) => {
-      const size = file.size;
-      if (size <= 5000000) {
-        valid = false;
-      }
-    });
-  }
-  return valid;
-}
 
-export function checkIfFilesAreCorrectType(files?: [File]): boolean {
-  let valid = true;
-  if (files) {
-    files.map((file) => {
-      if (!["application/pdf", "image/jpeg", "image/png"].includes(file.type)) {
-        valid = false;
-      }
-    });
-  }
-  return valid;
-}
 const AddBranch = () => {
   const initialValues = {
     BrancheName: "",
